09550fb7fcb263016c87231e51b469bce2536397
Suriyaa <suriyaaap13@gmail.com>
2022-03-07 22:52:00 +0530

    Level 2 Encryption

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect('mongodb://localhost:27017/userDB');
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = "Openyoureyes";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] } );
+
+const User = new mongoose.model("User", userSchema);
+
+
+app.get('/', function(req,res){
+    res.render('home');
+});
+
+app.route('/login')
+.get(function(req,res){
+    res.render('login');
+})
+.post(function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Successfully found the user");
+            if(foundUser.password === password){
+                console.log("logged in successfully");
+                res.render('secrets');
+            }else{
+                console.log("log in failed due to wrong password");
+                res.redirect('/login');
+            }
+        }
+    });
+});
+
+///// Register //////////////////////
+app.route('/register')
+.get(function(req,res){
+    res.render('register');
+})
+.post(function(req, res){
+    console.log(req.body);
+    const userData = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    userData.save(function(err){
+        if(err){
+            console.log("Error in saving the userData");
+        }else{
+            console.log("Successfully saved userData");
+        }
+    });
+    res.redirect('/login');
+});
+
+app.listen(3000, function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Server is up and running in port 3000");
+    }
+});
